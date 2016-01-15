6eb7203d0745684b9b884fd68b6b7b55e246e48b
srdjan-bozovic <srdjan.bozovic@live.com>
2016-01-15 08:50:23 +0100

    Updated docker-geekquiz-acs.ps1 in Azure Automation.

--- azure-runbooks/docker-geekquiz-acs.ps1
+++ azure-runbooks/docker-geekquiz-acs.ps1
@@ -1,10 +1,8 @@
 <#
     .SYNOPSIS 
-        Creates new RG and Dev QA enviroment for testing. Enviroment has Ubuntu Linux with needed Docker bits  
+        Deploys docekrized solution for testing to Mesos cluseter. Enviroment has Marathon and Chronos running on top of Mesos cluster  
     .DESCRIPTION
-        This sample runbooks creates fresh, new QA enviroment after Docker build initiates/triggers webhook
-        Note: If you do not uncomment the "Select-AzureSubscription" statement (on line 58) and insert
-        the name of your Azure subscription, the runbook will use the default subscription.
+        This sample runbooks deploys dockerized solution in to Mesos cluster, after Docker build initiates/triggers webhook
     .PARAMETER 
        No parameter required for this runbook
     .REQUIREMENTS 
@@ -12,26 +10,29 @@
         your Azure Automation instance.
         
     .NOTES
-        AUTHOR: Aleksandar Dordevic, @Alex_ZZ_
-        LASTEDIT: Nov 11, 2015
+        AUTHOR: Aleksandar Dordevic, @Alex_ZZ_, Srdjan Bozovic
+        LASTEDIT: Novanuary 15, 2016
 #>
 
 workflow docker-geekquiz-acs
 {
 
  #Getting json template for ACS
+ Write-Output 'Getting json template for ACS'                                 
  $template = (Invoke-WebRequest https://raw.githubusercontent.com/srdjan-bozovic/docker-geekquiz-acs/master/marathon-scripts/geekquiz.json -UseBasicParsing).Content
 
  #In case there is no previous installation
  try
  {
   #Clearing previous installation
-  Invoke-WebRequest -Uri http://acs-cph-cluster.westeurope.cloudapp:8080/v2/groups/geekquiz -Method Delete
+  Write-Output 'Clearing previous installation'                                 
+  Invoke-WebRequest -Uri http://acs-cph-cluster.westeurope.cloudapp.azure.com:8080/v2/groups/geekquiz -Method Delete -UseBasicParsing
  }
  catch
  {
  }
  
- #Deploy template                    
- Invoke-WebRequest -Uri http://acs-cph-cluster.westeurope.cloudapp:8080/v2/groups -Method Post -Headers @{"content-type"="application/json"} -Body $template       
+ #Deploying template   
+ Write-Output 'Deploying template'                                 
+ Invoke-WebRequest -Uri http://acs-cph-cluster.westeurope.cloudapp.azure.com:8080/v2/groups -Method Post -Headers @{"content-type"="application/json"} -Body $template -UseBasicParsing       
 }
